94d87f55205208cedefd8aa3846e641a61d1f1de
Daniel Ivanov <daniel.ivanov@me.com>
2023-04-28 13:28:25 +0200

    - init

---
+++ app/home/home.tsx
@@ -0,0 +1,25 @@
+import { Link, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Signup() {
+  const options = {
+    title: "Home",
+  };
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={options} />
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
